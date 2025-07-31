0ee6bbe90ad22c9b01975639a3c30185d4931a68
immmadeus <38637209+immmadeus@users.noreply.github.com>
2025-07-31 12:18:35 -0500

    these are the files you gave me, cody

---
+++ Circuit AnalyserTests/Circuit_AnalyserTests.swift
@@ -0,0 +1,17 @@
+//
+//  Circuit_AnalyserTests.swift
+//  Circuit AnalyserTests
+//
+//  Created by Penny on 7/16/25.
+//
+
+import Testing
+@testable import Circuit_Analyser
+
+struct Circuit_AnalyserTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
